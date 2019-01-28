5b5199826904d3f58e543e8aebd3b4268a6a783e
Alex Garibay <alex@alexgaribay.com>
2019-01-28 07:29:02 -0900

    Version bump to 0.1.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LibclusterGae.MixProject do
     [
       app: :libcluster_gae,
       description: "",
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
